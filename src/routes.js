253be17552305df92233d4beb8510ef929fd3acd
Ailton Savio <ailtons758@gmail.com>
2020-04-13 22:52:47 +0000

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,6 @@ const NewsController = require('./controllers/NewsController');
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
-
 // Mobile news index route
 routes.get('/news', NewsController.index);
 // Mobile read news route
